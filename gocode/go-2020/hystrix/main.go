091b252a709f714c9fe129e91059629e06090be7
陈洪彪 <chen.hongbiao@datatom.com>
2020-12-04 11:40:57 +0800

    fix: gjson测试

--- gocode/go-2020/hystrix/main.go
+++ gocode/go-2020/hystrix/main.go
@@ -11,11 +11,11 @@ var Result string
 
 func main() {
 	config := hystrix.CommandConfig{
-		Timeout:                1000,
-		MaxConcurrentRequests:  8,
-		RequestVolumeThreshold: 5,
-		SleepWindow:            1,
-		ErrorPercentThreshold:  30,
+		Timeout:                1000, // 超时时间设置  单位毫秒
+		MaxConcurrentRequests:  8,    // 最大请求数
+		SleepWindow:            1,    // 过多长时间，熔断器再次检测是否开启,单位毫秒
+		RequestVolumeThreshold: 5,    // 请求阈值  熔断器是否打开首先要满足这个条件；这里的设置表示至少有5个请求才进行ErrorPercentThreshold错误百分比计算
+		ErrorPercentThreshold:  30,   // 错误率 %
 	}
 	hystrix.ConfigureCommand("test", config)
 	cbs, _, _ := hystrix.GetCircuit("test")
@@ -32,7 +32,7 @@ func main() {
 
 func run() error {
 	Result = "RUNNING"
-	if Number > 10 {
+	if Number > 5 {
 		return nil
 	}
 	time.Sleep(2000 * time.Millisecond)
@@ -45,5 +45,6 @@ func run() error {
 
 func getFallBack(err error) error {
 	Result = "FALLBACK"
+	//fmt.Println("err:", err.Error())
 	return nil
 }
